f1c76f9e9de4a66aff60199bfe16c0cc049f0f81
Martin LEROY <mleroy70@myges.fr>
2025-03-14 14:27:41 +0100

    2

--- framework/sync.js
+++ framework/sync.js
@@ -3,6 +3,7 @@ const product = require('../models/product.model');
 const Catergory = require('../models/category.model');
 
 sync = async () => {
+    product.belongsTo(Catergory);
     await bdd.sync({ force: true });
 }
 
